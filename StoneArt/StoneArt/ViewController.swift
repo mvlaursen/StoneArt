036a747c645da30a2d8c5e75e1743727dd0f1066
Mike Laursen <mailbox@laursen.org>
2019-08-28 16:56:27 -0600

    Generated Swift project for iOS 9.3 or higher.

---
+++ StoneArt/StoneArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoneArt
+//
+//  Created by Mike Laursen on 8/28/19.
+//  Copyright © 2019 Appamajigger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
