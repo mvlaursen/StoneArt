2dce7f774affc6f9e5e4c616fd90b4c7549357a2
Mike Laursen <mailbox@laursen.org>
2019-08-29 17:57:28 -0600

    Added BoardMetrics.

--- StoneArt/StoneArt/View/BoardView.swift
+++ StoneArt/StoneArt/View/BoardView.swift
@@ -10,14 +10,41 @@ import SpriteKit
 import UIKit
 
 class BoardView: SKView {
+    struct BoardMetrics {
+        let boardImageName: String
+        let squareDim: CGFloat
+        let blackImageName: String
+        let whiteImageName: String
+    }
+    
     class BoardNode: SKSpriteNode {
+    }
+    
+    static func boardMetrics() -> BoardMetrics {
+        let size = UIScreen.main.bounds.size
+        let width = min(size.width, size.height)
+        
+        if width.isEqual(to: 375.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard368", squareDim: 23.0, blackImageName: "BlackStone21", whiteImageName: "WhiteStone21")
+        } else if width.isEqual(to: 768.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, blackImageName: "BlackStone45", whiteImageName: "WhiteStone45")
+        } else if width.isEqual(to: 414.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard400", squareDim: 25.0, blackImageName: "BlackStone23", whiteImageName: "WhiteStone23")
+        } else if width.isEqual(to: 1024.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard1024", squareDim: 64.0, blackImageName: "BlackStone60", whiteImageName: "WhiteStone60")
+        } else if width.isEqual(to: 834.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, blackImageName: "BlackStone45", whiteImageName: "WhiteStone45")
+        } else {
+            return BoardMetrics(boardImageName: "GomokuBoard320", squareDim: 20.0, blackImageName: "BlackStone18", whiteImageName: "WhiteStone18")
+        }
     }
     
     override func layoutSubviews() {
         if self.scene == nil {
+            let metrics = BoardView.boardMetrics()
             let scene = SKScene(size: self.bounds.size)
             
-            let board = BoardNode(imageNamed: "GomokuBoard1024")
+            let board = BoardNode(imageNamed: metrics.boardImageName)
             scene.addChild(board)
             self.presentScene(scene)
         }
