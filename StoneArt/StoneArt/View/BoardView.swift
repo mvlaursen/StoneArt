35c061ff06a26dfeb6f09c6e8a0ae5fc237258cc
Mike Laursen <mailbox@laursen.org>
2019-08-29 16:58:22 -0600

    Laid out Board View.

---
+++ StoneArt/StoneArt/View/BoardView.swift
@@ -0,0 +1,14 @@
+//
+//  BoardView.swift
+//  StoneArt
+//
+//  Created by Mike Laursen on 8/29/19.
+//  Copyright © 2019 Appamajigger. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class BoardView: SKView {
+
+}
