63f51cf1a5c3b3f25df5dd2b0edc472fa401ff36
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2019-09-02 13:26:32 -0600

    Feature/boardview (#1)
    * Copy assets from Gomoku project.
    
    * Laid out Board View.
    
    * Barely functional BoardView implementation
    
    * Added BoardMetrics.
    
    * Position board correctly in scene
    
    * Added title label.
    
    * Make board view background transparent.
    
    * More sophisticated label positioning

---
+++ StoneArt/StoneArt/View/BoardView.swift
@@ -0,0 +1,75 @@
+//
+//  BoardView.swift
+//  StoneArt
+//
+//  Created by Mike Laursen on 8/29/19.
+//  Copyright © 2019 Appamajigger. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class BoardView: SKView {
+    static let kBoardZPosition = CGFloat(100.0)
+    static let kSquaresPerDim = 15
+
+    struct BoardMetrics {
+        let boardImageName: String
+        let squareDim: CGFloat
+        let blackImageName: String
+        let whiteImageName: String
+    }
+    
+    class BoardNode: SKSpriteNode {
+    }
+    
+    static func boardMetrics() -> BoardMetrics {
+        let size = UIScreen.main.bounds.size
+        let width = min(size.width, size.height)
+        
+        if width.isEqual(to: 375.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard368", squareDim: 23.0, blackImageName: "BlackStone21", whiteImageName: "WhiteStone21")
+        } else if width.isEqual(to: 768.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, blackImageName: "BlackStone45", whiteImageName: "WhiteStone45")
+        } else if width.isEqual(to: 414.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard400", squareDim: 25.0, blackImageName: "BlackStone23", whiteImageName: "WhiteStone23")
+        } else if width.isEqual(to: 1024.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard1024", squareDim: 64.0, blackImageName: "BlackStone60", whiteImageName: "WhiteStone60")
+        } else if width.isEqual(to: 834.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, blackImageName: "BlackStone45", whiteImageName: "WhiteStone45")
+        } else {
+            return BoardMetrics(boardImageName: "GomokuBoard320", squareDim: 20.0, blackImageName: "BlackStone18", whiteImageName: "WhiteStone18")
+        }
+    }
+    
+    override func layoutSubviews() {
+        if self.scene == nil {
+            let metrics = BoardView.boardMetrics()
+            let scene = SKScene(size: self.bounds.size)
+            scene.backgroundColor = SKColor.clear
+            
+            // To make conversion from the location of a tap in the scene's
+            // coordinate system to board row and column as straightforward as
+            // possible, align row 0, column 0 of the board image with the
+            // scene's origin. There is still the difference that the scene's
+            // y-axis points up while row numbers increase from top to bottom
+            // of the board, but aligning row 0 with y = 0 allows us to simply
+            // negate the value of y when converting.
+            
+            let board = BoardNode(imageNamed: metrics.boardImageName)
+            
+            let squareDimInUnitSpace = 1.0 / CGFloat(BoardView.kSquaresPerDim + 1)
+            board.anchorPoint = CGPoint(x: squareDimInUnitSpace, y: 1.0 - squareDimInUnitSpace)
+            
+            // If our collection of artwork is done correctly, the board images
+            // always fit within the available screen space.
+            let xMargin = (scene.size.width - board.size.width) / 2.0
+            let yMargin = (scene.size.height - board.size.height) / 2.0
+            board.position = CGPoint(x: xMargin + metrics.squareDim, y: yMargin + CGFloat(BoardView.kSquaresPerDim) * metrics.squareDim)
+            board.zPosition = BoardView.kBoardZPosition
+            
+            scene.addChild(board)
+            
+            self.presentScene(scene)
+        }
+    }}
