806ac77a15e9f2a8c667f57916251746f9c3560b
Mike Laursen <mailbox@laursen.org>
2019-09-24 15:59:27 -0600

    Port touch gesture handling from Gomoku app.

--- StoneArt/StoneArt/View/BoardView.swift
+++ StoneArt/StoneArt/View/BoardView.swift
@@ -9,9 +9,16 @@
 import SpriteKit
 import UIKit
 
+extension CGFloat {
+    func matches(_ other: CGFloat, within tolerance: CGFloat) -> Bool {
+        return abs(self - other) < tolerance
+    }
+}
+
 class BoardView: SKView {
     static let kBoardZPosition = CGFloat(100.0)
     static let kStoneZPosition = CGFloat(200.0)
+    static let kTapTolerance = CGFloat(2.5)
 
     private static let kBoardUpdateInterval = 0.1
     
@@ -50,6 +57,8 @@ class BoardView: SKView {
         }
     }
     
+    // MARK: Layout and Refresh
+
     override func layoutSubviews() {
         if self.scene == nil {
             let metrics = BoardView.boardMetrics()
@@ -134,4 +143,67 @@ class BoardView: SKView {
         self.refresh()
         boardUpdateTimer = nil
     }
+
+    // MARK: Gesture Handling
+    
+    /**
+     * - parameter for: Should be in the SKScene's coordinate system.
+     */
+    private func moveIndex(for location: CGPoint) -> Int? {
+        var retVal: Int? = nil
+        
+        print("moveIndex: location.x, .y: \(location.x), \(location.y)")
+        
+        let metrics = BoardView.boardMetrics()
+        
+        // Convert the location of the tap to a row and column on the board.
+        //
+        // However, only return the row and column if the tap is not ambiguous
+        // (i.e. the tap is halfway between two rows or two columns, or the tap
+        // is in the board's margin). To check for ambiguity, reverse engineer
+        // the x and y coordinates of the tap from the row and column, then
+        // test if the reverse-engineered x and y are close to the tap location
+        // that was passed in.
+        
+        let column = Int(round(location.x / metrics.squareDim))
+        let xFromColumn = CGFloat(column) * metrics.squareDim
+        
+        if xFromColumn.matches(location.x, within: metrics.squareDim / BoardView.kTapTolerance) {
+            let row = Int(round(-location.y / metrics.squareDim))
+            let yFromRow = CGFloat(-row) * metrics.squareDim
+            
+            if yFromRow.matches(location.y, within: metrics.squareDim / BoardView.kTapTolerance) {
+                print("    x, y: \(xFromColumn), \(yFromRow)")
+                print("    row, column: \(row), \(column)")
+                retVal = Board.indexFrom(row: row, column: column)
+            }
+        }
+        
+        return retVal
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let scene = self.scene {
+            let location = touch.location(in: scene)
+            let nodes = scene.nodes(at: location)
+            let stones = nodes.filter { $0.isKind(of: BoardView.StoneNode.self) }
+            // If our code is working correctly, there should never be more
+            // than one stone in the same spot on the board.
+            assert(stones.count <= 1)
+            
+            if stones.isEmpty {
+                let boardNodes = nodes.filter { $0.isKind(of: BoardView.BoardNode.self) }
+                assert(boardNodes.count <= 1)
+                if boardNodes.count > 0 {
+                    if let boardNode = boardNodes.first {
+                        if let moveIndex = moveIndex(for: touch.location(in: boardNode)) {
+//                            previousBoard = board
+                            board = Board(board: board, index: moveIndex, square: .black)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
