c74105a39cdca46a12bc81293ad67b51b0e8cb2c
Mike Laursen <mailbox@laursen.org>
2019-12-19 10:44:17 -0700

    Add UI test for Privacy Policy view.

--- StoneArt/StoneArtTests/StoneArtTests.swift
+++ StoneArt/StoneArtTests/StoneArtTests.swift
@@ -113,11 +113,11 @@ class StoneArtTests: XCTestCase {
         XCTAssertEqual(game1, game2)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
