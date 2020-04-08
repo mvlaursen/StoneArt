f69ee2e1a857a010b17e8a2da8d52b42741db506
Mike Laursen <mailbox@laursen.org>
2020-04-07 21:59:39 -0600

    Dismiss alert when app resigns active.

--- StoneArt/StoneArt/View/UIViewController+Alert.swift
+++ StoneArt/StoneArt/View/UIViewController+Alert.swift
@@ -12,6 +12,15 @@ extension UIViewController {
     func alertWithOKAction(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // Apple's Human Interface Guidelines for alerts on iOS recommend
+        // dismissing any visible alerts when the user goes to the Home screen.
+        NotificationCenter.default.addObserver(self, selector: #selector(onWillResignActive), name: UIApplication.willResignActiveNotification, object: nil)
+        
         self.present(alertController, animated: true)
+    }
+    
+    @objc private func onWillResignActive() {
+        self.dismiss(animated: false, completion: nil)
     }
 }
